e0ead119ca7830a039175fd8f725397f35981418
Tom Habing <thabing@illinois.edu>
2012-11-30 22:35:40 -0600

    Initial commit for the MARCVocab library

---
+++ MARCVocabulary/MarcCountries.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Xml
+
+Public Class MarcCountries
+  Const CountriesXmlUrl As String = "http://www.loc.gov/standards/codelists/countries.xml"
+
+  Public Property Countries As Dictionary(Of String, String)
+
+  Private Shared _thisInst As MarcCountries
+
+
+  Protected Sub New()
+    Dim x As New XmlDocument()
+    x.Load(CountriesXmlUrl)
+
+    Countries = New Dictionary(Of String, String)
+
+    Dim xns As New XmlNamespaceManager(x.NameTable)
+    xns.AddNamespace("cc", "info:lc/xmlns/codelist-v1")
+
+    Dim nds = x.SelectNodes("//cc:country", xns)
+
+    For Each nd As XmlElement In nds
+      Dim nm As XmlElement = nd.SelectSingleNode("cc:name", xns)
+      Dim cd As XmlElement = nd.SelectSingleNode("cc:code", xns)
+
+      Countries.Add(cd.InnerText, nm.InnerText)
+    Next
+  End Sub
+
+
+  Public Shared Function Create() As MarcCountries
+    If _thisInst Is Nothing Then
+      _thisInst = New MarcCountries
+    End If
+
+    Return _thisInst
+  End Function
+
+  Public Shared Function GetName(code As String) As String
+    If _thisInst Is Nothing Then
+      _thisInst = New MarcCountries
+    End If
+
+    If code IsNot Nothing AndAlso _thisInst.Countries.ContainsKey(code) Then
+      Return _thisInst.Countries.Item(code)
+    Else
+      Return code
+    End If
+  End Function
+End Class
---
+++ MARCVocabulary/MarcLanguages.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Xml
+
+Public Class MarcLanguages
+  Const LanguagesXmlUrl As String = "http://www.loc.gov/standards/codelists/languages.xml"
+
+  Public Property Languages As Dictionary(Of String, String)
+
+  Private Shared _thisInst As MarcLanguages
+
+
+  Protected Sub New()
+    Dim x As New XmlDocument()
+    x.Load(LanguagesXmlUrl)
+
+    Languages = New Dictionary(Of String, String)
+
+    Dim xns As New XmlNamespaceManager(x.NameTable)
+    xns.AddNamespace("cc", "info:lc/xmlns/codelist-v1")
+
+    Dim nds = x.SelectNodes("//cc:language", xns)
+
+    For Each nd As XmlElement In nds
+      Dim nm As XmlElement = nd.SelectSingleNode("cc:name", xns)
+      Dim cd As XmlElement = nd.SelectSingleNode("cc:code", xns)
+
+      Languages.Add(cd.InnerText, nm.InnerText)
+    Next
+  End Sub
+
+  Public Shared Function Create() As MarcLanguages
+    If _thisInst Is Nothing Then
+      _thisInst = New MarcLanguages
+    End If
+
+    Return _thisInst
+  End Function
+
+  Public Shared Function GetName(code As String) As String
+    If _thisInst Is Nothing Then
+      _thisInst = New MarcLanguages
+    End If
+
+    If code IsNot Nothing AndAlso _thisInst.Languages.ContainsKey(code) Then
+      Return _thisInst.Languages.Item(code)
+    Else
+      Return code
+    End If
+  End Function
+
+End Class
