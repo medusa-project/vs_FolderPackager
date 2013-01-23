5fd919f033583176b55dc9a2b57589a373c2af38
Tom Habing <thabing@illinois.edu>
2013-01-22 22:53:11 -0600

    Code for returning and registering handles

---
+++ IdManager/IdManagementException.vb
@@ -0,0 +1,12 @@
+﻿Public Class IdManagementException
+  Inherits Exception
+
+  Public Sub New(ByVal Message As String)
+    MyBase.New(Message)
+  End Sub
+
+  Public Sub New(ByVal Message As String, ByVal InnerException As Exception)
+    MyBase.New(Message, InnerException)
+  End Sub
+
+End Class
--- IdManager/IdManager.vb
+++ IdManager/IdManager.vb
@@ -1,12 +1,100 @@
 ﻿Imports System.Security
 Imports System.Text.RegularExpressions
 Imports Uiuc.Library.Medusa
+Imports Uiuc.Library.Premis
+Imports Uiuc.Library.HandleClient
 
 ''' <summary>
 ''' A collection of shared functions for dealing with identifiers in the Medusa repository
 ''' </summary>
 ''' <remarks></remarks>
 Public Class IdManager
+
+  ''' <summary>
+  ''' Given the URI return the associated handle
+  ''' </summary>
+  ''' <param name="uri"></param>
+  ''' <returns>The handle or empty string if the uri is not found</returns>
+  ''' <remarks></remarks>
+  Public Shared Function GetHandle(uri As Uri) As String
+    Dim hndl As String = ""
+    Using db As New MedusaDataContext(MedusaAppSettings.Settings.ConnectionString)
+
+      hndl = (From ha In db.HandleActions Where ha.target = uri.ToString Order By ha.id Descending Select ha.handle).FirstOrDefault
+
+    End Using
+
+    Return hndl
+
+  End Function
+
+  ''' <summary>
+  ''' Register a handle for the file or folder object
+  ''' </summary>
+  ''' <param name="newFPath"></param>
+  ''' <param name="pObj"></param>
+  ''' <returns></returns>
+  ''' <remarks>This function requires the pContainer and pUserAgent be instantiated</remarks>
+  Public Shared Function RegisterFileHandle(newFPath As String, pObj As PremisObject, pContainer As PremisContainer, pUserAgent As PremisAgent, pSoftAgent As PremisAgent) As String
+    If pContainer Is Nothing Or pUserAgent Is Nothing Or pSoftAgent Is Nothing Then
+      Throw New IdManagementException("Object is not in state where a handle can be registered.")
+    End If
+
+    Dim urib As New UriBuilder
+    urib.Scheme = "file"
+    urib.Path = newFPath
+
+    'register a handle for the URI if not already done
+    Dim handle As String = IdManager.GetHandle(urib.Uri)
+    Dim local_id As String
+
+    If Not String.IsNullOrWhiteSpace(handle) Then
+      'use the already registered handle 
+      local_id = IdManager.ParseLocalIdentifier(handle)
+    Else
+      local_id = pObj.LocalIdentifierValue
+    End If
+
+    'Register a new handle for the file, if needed
+    Try
+      Dim hc As HandleClient.HandleClient = HandleClient.HandleClient.CreateUpdateHandle(local_id, urib.Uri.ToString, pUserAgent.EmailIdentifierValue, _
+                                                                                      String.Format("Collection: {0}", MedusaAppSettings.Settings.CollectionName))
+      handle = hc.handle_value
+      If Not IdManager.ValidateHandle(handle) Then
+        Trace.TraceError("Invalid Handle: " & handle)
+        Trace.Flush()
+        Throw New IdManagementException("Invalid Handle: " & handle)
+      End If
+
+      pObj.ObjectIdentifiers.Add(New PremisIdentifier("HANDLE", handle))
+
+      Dim hEvt As PremisEvent = hc.GetPremisEvent(New PremisIdentifier("LOCAL", pContainer.NextID))
+      hEvt.LinkToObject(pObj)
+      hEvt.LinkToAgent(pUserAgent)
+      hEvt.LinkToAgent(pSoftAgent)
+
+      pContainer.Events.Add(hEvt)
+
+    Catch ex As Exception
+      Trace.TraceError("Handle: {0}", ex.Message)
+      Trace.Flush()
+      MedusaHelpers.EmailException(ex)
+
+      Dim hEvt As New PremisEvent("LOCAL", pContainer.NextID, "HANDLE_CREATION")
+      Dim hEvtOut As New PremisEventOutcomeInformation("FAILED")
+      hEvtOut.EventOutcomeDetails.Add(New PremisEventOutcomeDetail(ex.Message))
+      hEvt.EventOutcomeInformation.Add(hEvtOut)
+      hEvt.LinkToObject(pObj)
+      hEvt.LinkToAgent(pUserAgent)
+      hEvt.LinkToAgent(pSoftAgent)
+
+      pContainer.Events.Add(hEvt)
+
+    End Try
+
+    Return handle
+
+  End Function
 
   ''' <summary>
   ''' Given the human-readable identifier return the corresponding medusa identifier, 
@@ -18,7 +106,7 @@ Public Class IdManager
   ''' <remarks></remarks>
   Public Shared Function GetMedusaIdentifier(humanId As String) As String
     Dim id As String = ""
-    Using db As New MedusaDataContext
+    Using db As New MedusaDataContext(MedusaAppSettings.Settings.ConnectionString)
 
       id = (From i In db.IdentifierLookups Where i.HumanIdentifier = humanId Select i.MedusaIdentifier).SingleOrDefault
 
@@ -31,6 +119,11 @@ Public Class IdManager
     Return id
   End Function
 
+  Public Shared Function GetLocalPremisIdentifier(humanId As String) As PremisIdentifier
+    Return New PremisIdentifier("LOCAL", IdManager.GetMedusaIdentifier(humanId))
+  End Function
+
+
   ''' <summary>
   ''' Given the medusa identifier return the  human-readable id
   ''' </summary>
@@ -39,7 +132,7 @@ Public Class IdManager
   ''' <remarks></remarks>
   Public Shared Function GetHumanIdentifier(medusaId As String) As String
     Dim id As String = ""
-    Using db As New MedusaDataContext
+    Using db As New MedusaDataContext(MedusaAppSettings.Settings.ConnectionString)
 
       id = (From i In db.IdentifierLookups Where i.MedusaIdentifier = medusaId Select i.HumanIdentifier).SingleOrDefault
 
@@ -57,7 +150,7 @@ Public Class IdManager
   ''' <remarks></remarks>
   Public Shared Function AddIdentifier(humanId As String, medusaId As String) As String
     Dim id As String = ""
-    Using db As New MedusaDataContext
+    Using db As New MedusaDataContext(MedusaAppSettings.Settings.ConnectionString)
 
       id = (From i In db.IdentifierLookups Where i.HumanIdentifier = humanId Select i.MedusaIdentifier).SingleOrDefault
 
@@ -105,7 +198,7 @@ Public Class IdManager
   ''' <param name="handle"></param>
   ''' <returns></returns>
   ''' <remarks></remarks>
-  Public Shared Function GetLocalIdentifier(handle As String) As String
+  Public Shared Function ParseLocalIdentifier(handle As String) As String
     Dim re As New Regex("^\s*(\d+)/([^:]+:[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}-[A-F0-9](?:\.\d+)?)\s*$", RegexOptions.IgnoreCase)
     Dim ret As String
 
